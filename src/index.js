5abd2e8fbec687f87d767b8b56a595b648960cee
gitMarcldo <marcldo@users.noreply.github.com>
2021-05-19 11:34:23 -0400

    useState: tracking multiple items

--- src/index.js
+++ src/index.js
@@ -4,13 +4,15 @@ import reportWebVitals from './reportWebVitals';
 
 const App = (props) => {
   const [count, setCount] = useState(props.count);
+  const [text, setText] = useState('');
 
   return (
     <div>
-      <p>The current count is {count}</p>
+      <p>The current {text || 'count'} is {count}</p>
       <button onClick={() => setCount(count + 1)}>+1</button>
       <button onClick={() => setCount(count - 1)}>-1</button>
       <button onClick={() => setCount(props.count)}>Reset</button>
+      <input value={text} onChange={(e) => setText(e.target.value)} />
     </div>
   )
 };

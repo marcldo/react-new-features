aa8833d5e45592fd1ead8458e376a83a8777773d
gitMarcldo <marcldo@users.noreply.github.com>
2021-05-27 11:09:19 -0400

    NoteApp component

--- src/index.js
+++ src/index.js
@@ -1,62 +1,9 @@
-import React, { useState, useEffect, useReducer } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
+import NoteApp from './components/NoteApp';
 
-import notesReducer from './reducers/notes';
-import Note from './components/Note';
 
-const NoteApp = () => {
-  const [notes, dispatch] = useReducer(notesReducer, []);
-  const [title, setTitle] = useState('');
-  const [body, setBody] = useState('');
-
-  useEffect(() => {
-    const notes = JSON.parse(localStorage.getItem('notes'));
-
-    if (notes) {
-      dispatch({ type: 'POPULATE_NOTES', notes });
-    }
-  }, []);
-
-  useEffect(() => {
-    localStorage.setItem('notes', JSON.stringify(notes));
-
-  }, [notes]);
-
-  const addNote = (e) => {
-    e.preventDefault();
-    dispatch({
-      type: 'ADD_NOTE',
-      title,
-      body
-    });
-
-    setTitle('');
-    setBody('');
-  };
-
-  const removeNote = (title) => {
-    dispatch({
-      type: 'REMOVE_NOTE',
-      title
-    });
-  }
-
-  return (
-    <div>
-      <h1>Notes</h1>
-      {notes.map((note) => (
-        <Note key={note.title} note={note} removeNote={removeNote} />
-      ))}
-      <p>Add Note</p>
-      <form onSubmit={addNote}>
-        <input value={title} onChange={(e) => setTitle(e.target.value)} />
-        <textarea value={body} onChange={(e) => setBody(e.target.value)} />
-        <button>add note</button>
-      </form>
-    </div>
-  );
-};
 
 ReactDOM.render(
   <React.StrictMode>

d0758675c3e3044f88996806ded57c034119bf0b
gitMarcldo <marcldo@users.noreply.github.com>
2021-05-27 11:42:31 -0400

    AddNoteForm component

--- src/components/NoteApp.js
+++ src/components/NoteApp.js
@@ -1,11 +1,10 @@
-import React, { useState, useEffect, useReducer } from 'react';
+import React, { useEffect, useReducer } from 'react';
 import notesReducer from '../reducers/notes';
 import NoteList from './NoteList';
+import AddNoteForm from './AddNoteForm';
 
 const NoteApp = () => {
   const [notes, dispatch] = useReducer(notesReducer, []);
-  const [title, setTitle] = useState('');
-  const [body, setBody] = useState('');
 
   useEffect(() => {
     const notes = JSON.parse(localStorage.getItem('notes'));
@@ -20,18 +19,6 @@ const NoteApp = () => {
 
   }, [notes]);
 
-  const addNote = (e) => {
-    e.preventDefault();
-    dispatch({
-      type: 'ADD_NOTE',
-      title,
-      body
-    });
-
-    setTitle('');
-    setBody('');
-  };
-
   const removeNote = (title) => {
     dispatch({
       type: 'REMOVE_NOTE',
@@ -43,12 +30,7 @@ const NoteApp = () => {
     <div>
       <h1>Notes</h1>
       <NoteList notes={notes} removeNote={removeNote} />
-      <p>Add Note</p>
-      <form onSubmit={addNote}>
-        <input value={title} onChange={(e) => setTitle(e.target.value)} />
-        <textarea value={body} onChange={(e) => setBody(e.target.value)} />
-        <button>add note</button>
-      </form>
+      <AddNoteForm dispatch={dispatch} />
     </div>
   );
 };

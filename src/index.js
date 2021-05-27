31c779dc912016483b418d362c110cee303131b5
gitMarcldo <marcldo@users.noreply.github.com>
2021-05-27 11:01:06 -0400

    notes reducer

--- src/index.js
+++ src/index.js
@@ -2,21 +2,7 @@ import React, { useState, useEffect, useReducer } from 'react';
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
 
-const notesReducer = (state, action) => {
-  switch (action.type) {
-    case 'POPULATE_NOTES':
-      return action.notes
-    case 'ADD_NOTE':
-      return [
-        ...state,
-        { title: action.title, body: action.body }
-      ]
-    case 'REMOVE_NOTE':
-      return state.filter((note) => note.title !== action.title)
-    default:
-      return state
-  };
-};
+import notesReducer from './reducers/notes';
 
 const NoteApp = () => {
   const [notes, dispatch] = useReducer(notesReducer, []);
